311193fe87f837c9e3b9c131a17ac76a0601664b
William Anajemba <Wjemba@gmail.com>
2023-08-17 15:47:14 -0700

    Write message with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This is the function that prints a message
+ * Return: The value of the fuction is returned to 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
