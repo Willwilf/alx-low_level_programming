f72de3cf7b411425a881fb19922ec028ee93764e
William Anajemba <Wjemba@gmail.com>
2023-08-17 14:48:59 -0700

    Write message with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - short description
+ * Return: The return value is 0
+ */
+int main(void)
+{
+	char Mystr[] = "Programming is like building a multingual puzzle";
+
+	puts(Mystr) ; /*Writing the string to stdout*/
+
+	return (0);
+}
+
