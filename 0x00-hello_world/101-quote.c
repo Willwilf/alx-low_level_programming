fe8134e165c1a94f456b1444b59faa9b11ae3173
William Anajemba <Wjemba@gmail.com>
2023-08-17 18:28:03 -0700

    unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Printsa a function that has a message
+ *
+ * Return: it will return the value of 1 
+ */
+int main(void)
+{
+	write(2,
+	"and that piece of art is useful" - Dora Korpar, 2015-10-19);
+
+	return (1);
+}
