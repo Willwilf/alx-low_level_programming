6d5b264454611bcf788da8434c0e26c3b01ac744
William Anajemba <Wjemba@gmail.com>
2023-08-17 17:31:49 -0700

    Sizeof various type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,8 @@ int main(void)
 	int intType;
 	float floatType;
 	char charType;
-	double doubleType;
+	long int longintType;
+	long long longlongType;
 
 	/**
 	 *sizeof evaluates the size of the variable
@@ -17,7 +18,8 @@ int main(void)
 	printf("size of int: %zu bytes \n", sizeof(intType));
 	printf("size of float: %zu bytes \n", sizeof(floatType));
 	printf("size of char: %zu bytes \n", sizeof(charType));
-	printf("size of double: %zu bytes \n", sizeof(doubleType));
+	printf("size of long int: %zu bytes \n", sizeof(longintType));
+	printf("size of long long: %zu bytes \n", sizeof(longlongType));
 
 	return (0);
 }
