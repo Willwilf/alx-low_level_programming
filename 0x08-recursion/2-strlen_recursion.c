a17e3082aea98b286687e7549de2892a1e7d4b75
Willwilf <wjemba@gmail.com>
2023-11-26 14:00:48 -0800

    prints length of string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,11 +10,11 @@ int _strlen_recursion(char *s)
 	{
 		return (0);
 	}
+	else
 	{
 		int len = 0;
 
-		len++;
-		len += _strlen_recursion(s + 1);
+		len = 1 + _strlen_recursion(s + 1);
 	}
 	return (len);
 }
