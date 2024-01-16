15467b1cbff660756d5848061a27a29f0dd76bd5
Willwilf <wjemba@gmail.com>
2024-01-16 13:08:33 -0800

    My library

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isdigit - is a function that checks for the digit from 0 - 9 and returns a
+ * value of 1 and returns a  value of 0 for the rest
+ *
+ * @c: the character in ASCII code
+ *
+ * Return: 1 for the digits from 0 - 9 and returns 0 for the rest.
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
