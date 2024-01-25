f0cb315a462550023100ec95728c94e0b5aaa594
Willwilf <wjemba@gmail.com>
2024-01-25 05:59:23 -0800

    Array of characters

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - create array of size size and assign char c
+ * Description: creates array of size size and assign char c
+ * @size: the size of the memory required
+ * @c: the placeholder
+ * Return: pointer to the array, NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	char *PArray;
+	unsigned int ife;
+
+	PArray = malloc(size * sizeof(char));
+	if (PArray == NULL)
+	{
+		return (NULL);
+	}
+	for (ife = 0; ife < size; ife++)
+	{
+		PArray[ife] = c;
+	}
+
+	return (PArray);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,5 @@
+Malloc
+
+it is better known as dynamic memory allocation,this is a function 
+that allows us to use functions to determine the amount of memory allocated
+when a program is executed
